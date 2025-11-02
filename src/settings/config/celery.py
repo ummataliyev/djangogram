cb745f6e9c546081e4722f1b754a1d0e7328fd29
ummataliyev <djohnummataliyev@gmail.com>
2025-11-02 20:55:47 +0500

    schedule for celery

--- src/settings/config/celery.py
+++ src/settings/config/celery.py
@@ -4,17 +4,19 @@ from celery import Celery
 
 from datetime import timedelta
 
+from src.settings.config.configs import config
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 app = Celery("Djangogram")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     "notify": {
         "task": "apps.bot.tasks.notify.send_hi_to_all_users",
-        "schedule": timedelta(seconds=2),
+        "schedule": timedelta(minutes=config.CELERY_NOTIFY_INTERVAL),
     },
 }
 
--- src/settings/config/configs.py
+++ src/settings/config/configs.py
@@ -26,6 +26,7 @@ class Config:
         self.CELERY_RESULT_BACKEND = env.str("CELERY_RESULT_BACKEND")
         self.CELERY_BEAT_SCHEDULER = env.str("CELERY_BEAT_SCHEDULER")
         self.CELERY_TIMEZONE = env.str("CELERY_TIMEZONE")
+        self.CELERY_NOTIFY_INTERVAL = env.str("CELERY_NOTIFY_INTERVAL")
 
         if not self.DB_URL:
             self.DB_URL = self.generate_db_url()
